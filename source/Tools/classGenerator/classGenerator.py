0381dac7abdf90ec2c4a7a4442baaaedefcb679b
velter17 <velter17@bk.ru>
2016-11-03 23:14:43 +0200

    Provide class generator script

---
+++ source/Tools/classGenerator/classGenerator.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import os
+import argparse
+import datetime
+
+
+def main():
+    parser = argparse.ArgumentParser(description = "Graviz class generator")
+    parser.add_argument("-p", "--path", help="path for header file")
+    parser.add_argument("-n", "--name", help="name of class")
+    parser.add_argument("-a", "--author", default="Dmytro Sadovyi", help="author of code")
+    parser.add_argument("-s", "--namespace", default="none", help="Wrap into namespace, default is none")
+    args = parser.parse_args()
+    header = open("CClass.h.gen")
+    classHeader = open(args.path + "C" + args.name + ".h", 'w')
+    replaceList = [
+            ["$CLASS_NAME$","C" + args.name],
+            ["$FILE_NAME$", "C" + args.name + ".h"],
+            ["$AUTHOR$", args.author],
+            ["$DATE$", datetime.date.today().strftime("%d.%m.%Y")]
+            ]
+    if args.namespace != "none":
+        replaceList.append(["$NAMESPACE$", "N" + args.namespace])
+    for line in header:
+        for replaceItem in replaceList:
+            line = line.replace(replaceItem[0], replaceItem[1])
+        if args.namespace == "none" and line.startswith("$N"):
+            continue
+        elif line[:2] == "$N":
+            line = line[2:]
+        classHeader.write(line)
+
+    source = open("CClass.cpp.gen")
+    if not os.path.exists(args.path + "private/"):
+        os.makedirs(args.path + "private/")
+    classSource = open(args.path + "private/C" + args.name + ".cpp", "w")
+    replaceList[1][1] = "C" + args.name + ".cpp"
+    replaceList.append(["$HEADER_PATH$", "../C" + args.name + ".h"])
+    for line in source:
+        for replaceItem in replaceList:
+            line = line.replace(replaceItem[0], replaceItem[1])
+        if args.namespace == "none" and line.startswith("$N"):
+            continue
+        elif line[:2] == "$N":
+            line = line[2:]
+        classSource.write(line)
+
+
+if __name__ == "__main__":
+    main()
